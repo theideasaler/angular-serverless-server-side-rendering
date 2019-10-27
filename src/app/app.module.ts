a6c7eec466487b8fc1871cd71968b623f9b19559
DESKTOP-1KE2VOK\19545 <theideasaler@gmail.com>
2019-10-27 16:36:22 +1100

    update CSR to SSR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { AppRoutingModule } from './app-routing.module';
     ContactUsComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule
   ],
   providers: [],
