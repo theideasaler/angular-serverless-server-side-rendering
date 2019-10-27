cfeeafe65469546cf7a23f79d38e5ad4dc858f5b
DESKTOP-1KE2VOK\19545 <theideasaler@gmail.com>
2019-10-27 15:07:12 +1100

    add home and contact-us components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule
